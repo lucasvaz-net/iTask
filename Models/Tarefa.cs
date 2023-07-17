6681e7a22b2ce98cc8a7c5796ea06f137008bc0b
Lucas Vaz <110433221+lucasvaz-net@users.noreply.github.com>
2023-07-17 14:09:53 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Tarefa.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace iTask.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public int Prioridade { get; set; }
+        public string CategoriaId { get; set; }
+        public string StatusId { get; set; }
+        public string UsuarioId { get; set; }
+    }
+}
